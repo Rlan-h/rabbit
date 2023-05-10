8669ed76376f8f751115c6fc4a7391877987851a
Rlan-h <12790198+rlan-h@user.noreply.gitee.com>
2023-05-10 21:13:54 +0800

    home-图片懒加载指令优化

--- src/main.js
+++ src/main.js
@@ -7,30 +7,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { useIntersectionObserver } from '@vueuse/core'
+// 引入懒加载指令插件并且注册
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
 
 // 定义全局指令
-
-app.directive('img-lazy', {
-  mounted(el, binding) {
-    // el: 指定绑定的那个元素
-    // binding: binding.value 指令等于号后面绑定的表达式的值
-    console.log(el, binding.value)
-    const { stop } = useIntersectionObserver(
-      el,
-      ([{ isIntersecting }]) => {
-        if (isIntersecting) {
-          // 进入视口区域
-          el.src = binding.value
-        }
-      },
-    )
-  }
-})
