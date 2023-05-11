58192a6fedb21a28fb8e5568cf78090c97e851e8
Rlan-h <12790198+rlan-h@user.noreply.gitee.com>
2023-05-11 16:48:43 +0800

    详情页-全局组件插件化

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+// 把components下的组件都进行全局注册
+// 通过插件的方式
+
+
+import ImageView from './ImageView/index.vue'
+import Sku from './XtxSku/index.vue'
+
+export const componentPlugin = {
+  install(app) {
+    // app.component('组件的名字', 组件配置对象)
+    app.component('XtxImageView', ImageView)
+    app.component('XtxSku', Sku)
+  }
+}
