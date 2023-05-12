f09574a240e30326f43ff1bbfcece646d42db3e7
Rlan-h <12790198+rlan-h@user.noreply.gitee.com>
2023-05-12 14:37:48 +0800

    本地-头部购物车-删除功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -21,8 +21,17 @@ export const useCartStore = defineStore('cart', () => {
     }
   }
 
+  // 删除购物车
+  const delCart = (skuId) => {
+    // 找到要删除的项的下标值，- splice
+    // 或者使用数组的过滤方法 - filter
+    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+    cartList.value.splice(idx, 1)
+  }
+
   return {
     cartList,
-    addCart
+    addCart,
+    delCart
   }
 }, { persist: true })
